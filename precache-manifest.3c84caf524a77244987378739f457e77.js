4db414e928107adc7a93ed21c8c492a6d5dfe920
Adam Lewenhaupt <adam@sicada.se>
2018-12-08 12:23:34 +0100

    x

---
+++ precache-manifest.3c84caf524a77244987378739f457e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99454f0cc2137703aa5f",
+    "url": "/static/js/main.99454f0c.chunk.js"
+  },
+  {
+    "revision": "8a64a131602958539fd8",
+    "url": "/static/js/1.8a64a131.chunk.js"
+  },
+  {
+    "revision": "99454f0cc2137703aa5f",
+    "url": "/static/css/main.31d4687a.chunk.css"
+  },
+  {
+    "revision": "3966263d030eab7775d503b16ef6e66a",
+    "url": "/index.html"
+  }
+];
